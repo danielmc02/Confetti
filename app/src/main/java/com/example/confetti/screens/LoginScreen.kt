6cffddd676c85829cc380ec96191f31f72823938
Daniel <digity63@gmail.com>
2022-03-03 09:13:55 -0800

    Confetti / Updated

--- app/src/main/java/com/example/confetti/screens/LoginScreen.kt
+++ app/src/main/java/com/example/confetti/screens/LoginScreen.kt
@@ -3,25 +3,24 @@ package com.example.confetti.screens
 
 import android.content.res.Configuration.UI_MODE_NIGHT_NO
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.fontResource
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
-import com.example.confetti.screens.login_components.LoginComposable
-import com.example.confetti.screens.login_components.UserNameTextField
+import com.example.confetti.R
+import com.example.confetti.screens.login_components.LoginFormComposable
 import com.example.confetti.ui.theme.ConfettiTheme
 
+val CustomFont = FontFamily(
+    Font(R.font.chewy_regular , weight = FontWeight.Light)
+)
 
 @Composable
 fun LoginScreen(modifier: Modifier, ){
@@ -35,32 +34,23 @@ fun LoginScreen(modifier: Modifier, ){
             Row(
                 modifier
                     .fillMaxWidth()
-                    .weight(.2f),
-                verticalAlignment = Alignment.CenterVertically,
+                    .weight(.4f),
+                verticalAlignment = Alignment.Bottom,
                 horizontalArrangement = Arrangement.Center) {
                 //Top-Title
                 Text(text = "Confetti",
-                    fontSize = 40.sp,
+                    fontSize = 80.sp,
                     color = MaterialTheme.colors.onBackground,
+                    fontFamily = CustomFont
                 )
 
             }
             Column(
                 modifier
                     .fillMaxWidth()
-                    .weight(.7f)) {
+                    .weight(.3f)) {
                 //Body for sign in components
-                Row(
-                    modifier
-                        .fillMaxWidth()
-                        .weight(.1f)
-                        .fillMaxWidth(),
-                    verticalAlignment = Alignment.Bottom,
-                    horizontalArrangement = Arrangement.Start) {
-                    //Title for login
-                    Text(text = "Login", fontSize = 32.sp, color = MaterialTheme.colors.onBackground)
-                    
-                }
+
                 Column(
                     modifier
                         .fillMaxWidth()
@@ -69,39 +59,18 @@ fun LoginScreen(modifier: Modifier, ){
                     horizontalAlignment = Alignment.CenterHorizontally
                         ) {
                     //Body for text fields
-                    LoginComposable()
+                    LoginFormComposable()
                 }
 
             }
             Row(
                 modifier
                     .fillMaxWidth()
-                    .weight(.1f),
+                    .weight(.2f),
             verticalAlignment = Alignment.Bottom,
             horizontalArrangement = Arrangement.Start) {
                 //Dont have an account, sign up
-                Text(text = buildAnnotatedString {
-                       withStyle(
-                            style = SpanStyle(
-                                textDecoration = TextDecoration.None,
-                                color = MaterialTheme.colors.onBackground.copy(alpha = .8f)
-                            )
-                       ){
-                           append("Don't have an account?")
-                       }
-                    withStyle(
-                        style = SpanStyle(
-                            textDecoration = TextDecoration.None,
-                            color = MaterialTheme.colors.onBackground
-                        )
-                    ){
-                        append(" Sign up")
-                    }
-                },
-                    fontSize = 20.sp,
-                    color = MaterialTheme.colors.onBackground,
-                    modifier = Modifier.clickable {  }
-                )
+
             }
         }
     }
--- app/src/main/java/com/example/confetti/screens/login_components/UsernameAndPassword.kt
+++ app/src/main/java/com/example/confetti/screens/login_components/UsernameAndPassword.kt
@@ -1,58 +1,110 @@
 package com.example.confetti.screens.login_components
 
-import androidx.annotation.ColorRes
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Color.Companion.Magenta
 import androidx.compose.ui.graphics.Color.Companion.White
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.confetti.R
+import com.example.confetti.ui.theme.ConfettiTheme
 import com.example.confetti.ui.theme.Grey
-import com.example.confetti.ui.theme.WhiteOnBackGround
+
 
 
 @Composable
-fun LoginComposable(){
-    Column(modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.SpaceBetween,
-    horizontalAlignment = Alignment.CenterHorizontally) {
-        Surface(elevation = 6.dp,
-            shape = RoundedCornerShape(20.dp),
-            color = MaterialTheme.colors.primaryVariant,
-            modifier = Modifier
-                .width(300.dp)
-                .height(130.dp)
-        ){
-            Column(modifier = Modifier) {
-                Row(modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(.5f)) {
-                    UserNameTextField()
+fun LoginFormComposable() {
+
+    ConfettiTheme() {
+
+        Column(
+            modifier = Modifier.fillMaxWidth(), verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Surface(
+                elevation = 6.dp,
+                border = BorderStroke(width = 2.dp, brush = Brush.horizontalGradient(listOf(Color.Green,Color.Magenta))),
+                shape = RoundedCornerShape(20.dp),
+                color = MaterialTheme.colors.primaryVariant,
+                modifier = Modifier
+                    .width(300.dp)
+                    .height(130.dp)
+            ) {
+                Column(modifier = Modifier) {
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(.5f)
+                    ) {
+                        UserNameTextField()
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .background(Grey)
+                            .weight(.005f)
+                    ) {
+
+                    }
+                    Row(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .weight(.5f)
+                    ) {
+                        PasswordTextField()
+                    }
                 }
-                Row(modifier = Modifier.fillMaxWidth().background(Grey).weight(.005f)) {
-                    
-                }
-                Row(modifier = Modifier
-                    .fillMaxWidth()
-                    .weight(.5f)) {
-                    PasswordTextField()
-                }
+
             }
 
+            Spacer(modifier = Modifier.height(50.dp))
+            LoginButton()
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(text = buildAnnotatedString {
+                withStyle(
+                    style = SpanStyle(
+                        textDecoration = TextDecoration.None,
+                        color = MaterialTheme.colors.onBackground.copy(alpha = .8f)
+                    )
+                ){
+                    append("Don't have an account?")
+                }
+                withStyle(
+                    style = SpanStyle(
+                        textDecoration = TextDecoration.None,
+                        color = MaterialTheme.colors.onBackground,
+
+                    )
+                ){
+                    append(" Sign up")
+                }
+            },
+                fontSize = 15.sp,
+                color = MaterialTheme.colors.onBackground,
+                modifier = Modifier.clickable {  }
+            )
         }
-        Spacer(modifier = Modifier.height(50.dp))
-        LoginButton()
+
     }
-
 }
-
 
 @Preview
 @Composable
@@ -62,6 +114,6 @@ fun fulsce(){
         .background(
             White
         )) {
-        LoginComposable()
+        LoginFormComposable()
     }
 }
