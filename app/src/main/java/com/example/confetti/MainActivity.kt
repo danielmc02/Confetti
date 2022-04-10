1387dd70fa9671420ec38533662002307597c97a
Daniel <digity63@gmail.com>
2022-04-09 19:11:46 -0700

    Confetti / Updated

--- app/src/main/java/com/example/confetti/MainActivity.kt
+++ app/src/main/java/com/example/confetti/MainActivity.kt
@@ -5,11 +5,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material.ExperimentalMaterialApi
 import com.example.confetti.ui.theme.ConfettiTheme
+import com.google.firebase.auth.FirebaseAuth
 
 @ExperimentalMaterialApi
 class MainActivity : ComponentActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ConfettiTheme {
                 // A surface container using the 'background' color from the theme
